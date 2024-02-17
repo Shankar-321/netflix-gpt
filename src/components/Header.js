7533f66ec7fd7b1ab013f87c5b34839bf3f53ad9
Shankar-321 <rallapalliumashankar321@gmail.com>
2024-02-17 19:46:00 +0530

    confirm authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,9 @@ const Header = () => {
             <div className="flex p-2 ">
                 <img
                     className="w-12 h-12 rounded-sm "
+                    alt="logo"
                     src="https://occ-0-3433-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABa0E9eop5T2vtihjVGzaar3ENnWTpEdroljEdb_krS3Xc8X1hhHGKRC_yxJu3M9tOMA-Bv7TLr6kvck8pOiSbqD2eP1YYhk0kZ_JggrxMNs47OtQSYM5viaieVwwB3r3SgfJ64cUSRg8Fg.png?r=e6e" />
+
                 <button
                     onClick={handleSignOut}
                     className="bg-white font-bold border border-black p-2 m-1 hover:bg-red-300 rounded-lg m-2">
